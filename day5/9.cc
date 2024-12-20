eef5140362c30404dd598b6f77a00d606ccdc3c9
Vinyeta <avinetam@gmail.com>
2024-12-20 18:45:52 +0100

    some work on fifth day

--- day5/9.cc
+++ day5/9.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <regex>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
@@ -15,6 +16,13 @@ int main() {
         while (getline(in, line)) {
             lineSize = line.size();
             data.push_back(line);
+        }
+
+        vector<string>::iterator it = find(data.begin(), data.end(), "");
+        int separator = distance(data.begin(), it);
+        
+        for (int i = separator +1; i < data.size(); i++) {
+            if()
         }
         
     }
